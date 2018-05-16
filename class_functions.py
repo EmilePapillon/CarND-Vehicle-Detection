375c5d57cfe93255ba9f1b72b4693e5b560ab366
Emile Papillon Home <epapillon@live.ca>
2018-05-15 23:34:58 -0400

    good advancement, got promising results and made a process_frame function to process video frames

--- class_functions.py
+++ class_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 from skimage.feature import hog
+from scipy.ndimage.measurements import label
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
@@ -21,11 +23,11 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        block_norm= 'L2-Hys',
                        transform_sqrt=True, 
                        visualise=vis, feature_vector=feature_vec)
-        return features
+        return features.astype(np.float32)
 
 def bin_spatial(img, size=(32, 32)):
     # Use cv2.resize().ravel() to create the feature vector
-    features = cv2.resize(img, size).ravel() 
+    features = cv2.resize(img, size).ravel().astype(np.float32) 
     # Return the feature vector
     return features
 
@@ -40,7 +42,7 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     # Concatenate the histograms into a single feature vector
     hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
     # Return the individual histograms, bin_centers and feature vector
-    return channel1_hist, channel2_hist, channel3_hist, bin_centers,  hist_features
+    return channel1_hist, channel2_hist, channel3_hist, bin_centers,  hist_features.astype(np.float32)
 
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
@@ -51,5 +53,39 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return imcopy
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, heatmap):
+    labels = label(heatmap)
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
+
 
 
